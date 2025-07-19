2f3729f2f44f40fa63c61253f74ad43eb7977648
aramirezm <aramirez.unimarc@gmail.com>
2025-07-18 23:18:23 -0400

    fix

--- src/components/ui/sidebar/SideBar.tsx
+++ src/components/ui/sidebar/SideBar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useUiStore } from "@/store/uiStore";
 
 import { IoCloseOutline, IoSearch } from "react-icons/io5";
@@ -8,7 +8,6 @@ import { IoCloseOutline, IoSearch } from "react-icons/io5";
 export const SideBar = () => {
   const uiStore = useUiStore();
 
-
   return (
     <div
       className={`fixed inset-0 z-5 transform transition-transform duration-300 ease-in-out ${
@@ -16,12 +15,9 @@ export const SideBar = () => {
       }`}
     >
       {/* Fondo oscuro */}
-     
-        <>
-          <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30" />
-          <div className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-blur-sm" />
-        </>
-   
+
+      <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30" />
+      <div className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-blur-sm" />
 
       {/* Sidebar con animación */}
       <nav
@@ -36,14 +32,12 @@ export const SideBar = () => {
         />
 
         <div className="relative mt-14">
-          
-            <IoSearch size={20} className="absolute top-2  left-1 " />
-            <input
+          <IoSearch size={20} className="absolute top-2  left-1 " />
+          <input
             type="text"
-              className="w-full bg-gray-50 rounded pl-7 pr-5 py-1 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500"
-              placeholder=" "
-            />
-     
+            className="w-full bg-gray-50 rounded pl-7 pr-5 py-1 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500"
+            placeholder=" "
+          />
         </div>
       </nav>
     </div>
